96fa4b3b2da39491ff2b6ff2df6b95f124aa4983
Nicolas PATINELLA <PSR09678@students.ephec.be>
2019-12-29 21:20:40 +0100

    Adding router

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
-  
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
-  return(
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className='background-image'
       style={{
@@ -17,8 +19,6 @@ const MenuItem = ({ title, imageUrl, size }) => {
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-)}
+);
 
-
-
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>, document.getElementById('root'));
