8ea36305839110b6ba58504caeab1725c39393ac
Nicolas PATINELLA <PSR09678@students.ephec.be>
2019-12-30 00:14:23 +0100

    Adding header and link to shop and contact

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
-
+import Header from '../src/component/header/header.component';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
